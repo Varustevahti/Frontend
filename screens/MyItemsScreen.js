19e9f35ed5862638c5f64c90b5282b4f8ed6d88a
Jeremias <kormyy@hotmail.com>
2025-09-09 21:23:24 +0300

    Initialized App.js and MyItemsScreen

---
+++ screens/MyItemsScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+
+export default function MyItemsScreen() {
+    const [activeLocation, setActiveLocation] = useState(null);
+    const [activeCategory, setActiveCategory] = useState(null);
+
+    return (
+        <FlatList />
+
+    );
+}
+const styles = StyleSheet.create ({
+
+});
+  
