2b0098a67c499c0ae08119285072f853b248532c
Hirvilampi <lampinen.timo@gmail.com>
2025-09-18 23:46:07 +0300

    small additions to additemscreen.js and app.js. now using react-native-paper

--- App.js
+++ App.js
@@ -10,69 +10,72 @@ import AddItemScreen from "./screens/AddItemScreen";
 import SearchScreen from "./screens/SearchScreen";
 import GroupsScreen from "./screens/GroupsScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import { PaperProvider } from "react-native-paper";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            switch (route.name) {
-              case 'My Items':
-                return <Foundation name="home" size={size} color={color} />;
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              switch (route.name) {
+                case 'My Items':
+                  return <Foundation name="home" size={size} color={color} />;
 
-              case 'Add Item':
-                return <Octicons name="diff-added" size={size} color={color} />;
+                case 'Add Item':
+                  return <Octicons name="diff-added" size={size} color={color} />;
 
-              case 'Search':
-                return <Ionicons name="search" size={size} color={color} />;
+                case 'Search':
+                  return <Ionicons name="search" size={size} color={color} />;
 
-              case 'Groups':
-                return <Ionicons name="people" size={size} color={color} />;
+                case 'Groups':
+                  return <Ionicons name="people" size={size} color={color} />;
 
-              case 'Profile':
-                return <Ionicons name="person" size={size} color={color} />;
+                case 'Profile':
+                  return <Ionicons name="person" size={size} color={color} />;
 
-              default:
-                return null;
-            }
+                default:
+                  return null;
+              }
 
-          },
+            },
 
-          sceneContainerStyle: { backgroundColor: "#F8FBFA" },
-          headerStyle: {
-            backgroundColor: "#F8FBFA",
-            elevation: 0,
-            shadowOpacity: 0,
-            borderBottomWidth: 0,
-          },
-          headerTintColor: "#0D1A12",
+            sceneContainerStyle: { backgroundColor: "#F8FBFA" },
+            headerStyle: {
+              backgroundColor: "#F8FBFA",
+              elevation: 0,
+              shadowOpacity: 0,
+              borderBottomWidth: 0,
+            },
+            headerTintColor: "#0D1A12",
 
-          tabBarStyle: {
-            backgroundColor: "#F8FBFA",
-            height: 120,
-            paddingBottom: 10,
-            paddigTop: 10,
-          },
-          tabBarItemStyle: {
-            paddingVertical: 15,
-          },
-          tabBarLabelStyle: {
-            marginBottom: 10,
-          },
-          tabBarActiveTintColor: "#0D1A12",
-          tabBarInactiveTintColor: "#52946B",
-        })}
-      >
-        <Tab.Screen name="My Items" component={MyItemsScreen} />
-        <Tab.Screen name="Add Item" component={AddItemScreen} />
-        <Tab.Screen name="Search" component={SearchScreen} />
-        <Tab.Screen name="Groups" component={GroupsScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer >
+            tabBarStyle: {
+              backgroundColor: "#F8FBFA",
+              height: 120,
+              paddingBottom: 10,
+              paddigTop: 10,
+            },
+            tabBarItemStyle: {
+              paddingVertical: 15,
+            },
+            tabBarLabelStyle: {
+              marginBottom: 10,
+            },
+            tabBarActiveTintColor: "#0D1A12",
+            tabBarInactiveTintColor: "#52946B",
+          })}
+        >
+          <Tab.Screen name="My Items" component={MyItemsScreen} />
+          <Tab.Screen name="Add Item" component={AddItemScreen} />
+          <Tab.Screen name="Search" component={SearchScreen} />
+          <Tab.Screen name="Groups" component={GroupsScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer >
+    </PaperProvider>
   );
 }
 
